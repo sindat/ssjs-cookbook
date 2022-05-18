62c5396850e0bf0fd78801e10c462a45b88d9039
David Vondrovic <david.vondrovic@gmail.com>
2022-05-18 17:28:58 +0200

    ssjs API call

---
+++ ssjs_oauth2_http_get.js
@@ -0,0 +1,11 @@
+/*****************************************************************************
+Authorization header setup in case the API requires authorization header token (Oauth2)
+*****************************************************************************/
+
+// Ideally I would have the token stored as an env variable somewhere for security
+// var mytoken = "sometokenvalue"
+
+// var headerNames = ["Accept", "Authorization"];
+// var headerValues = ["application/json", "Bearer "+mytoken];
+
+// var response = HTTP.Get(url, headerNames, headerValues);
